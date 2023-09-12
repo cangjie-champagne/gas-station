d6aac021e1b96414d067b9dd3acf66ed0c50f1ff
João Pedro <joaopjt@icloud.com>
2023-09-12 20:43:16 -0300

    added k() method values

--- src/index.js
+++ src/index.js
@@ -496,7 +496,7 @@ class GasStation {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bow'],
       web: []
     };
     
@@ -512,19 +512,19 @@ class GasStation {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['fire'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['twenty'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['bow'];
           break;
       }
     }
