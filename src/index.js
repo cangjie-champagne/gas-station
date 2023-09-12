5554d8296406b57242b1c3593cf19dd7be3aef46
João Pedro <joaopjt@icloud.com>
2023-09-12 20:35:01 -0300

    added e() method values

--- src/index.js
+++ src/index.js
@@ -260,7 +260,7 @@ class GasStation {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['hand'],
       web: []
     };
     
@@ -276,19 +276,19 @@ class GasStation {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['middle'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['dagger-axe'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['field'];
           break;
       }
     }
