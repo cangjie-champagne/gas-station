417f90f0cac52a2ca79dea49041ebfa29a2e3eef
João Pedro <joaopjt@icloud.com>
2023-09-12 22:02:18 -0300

    added u() method values

--- src/index.js
+++ src/index.js
@@ -892,7 +892,7 @@ class GasStation {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['field'],
       web: []
     };
     
@@ -908,19 +908,19 @@ class GasStation {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['earth'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['wood'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['people'];
           break;
       }
     }
