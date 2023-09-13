080c975de4ff2e8f3324127e02172895b5168da7
João Pedro <joaopjt@icloud.com>
2023-09-12 21:08:43 -0300

    added p() method values

--- src/index.js
+++ src/index.js
@@ -692,7 +692,7 @@ class GasStation {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['water'],
       web: []
     };
     
@@ -708,19 +708,19 @@ class GasStation {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['hand'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['gold'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['day'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['one'];
           break;
       }
     }
