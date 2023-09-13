ea2d2571d85d5a3653081c7cc7131c5434baa003
João Pedro <joaopjt@icloud.com>
2023-09-12 22:07:09 -0300

    added y() method values

--- src/index.js
+++ src/index.js
@@ -1044,7 +1044,7 @@ class GasStation {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['bamboo'],
       web: []
     };
     
@@ -1060,19 +1060,19 @@ class GasStation {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['mountain'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['one'];
           break;
       }
     }
